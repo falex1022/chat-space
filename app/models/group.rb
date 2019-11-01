48c7fa3c27f2cde080e9a2753e5717583f4e71c8
falex1022 <56875497+falex1022@users.noreply.github.com>
2019-11-01 15:09:00 +0900

    chat-space_message

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,13 @@
 class Group < ApplicationRecord
+  has_many :messages
   has_many :user_groups
   has_many :users, through: :user_groups
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.content? ? last_message.content : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
