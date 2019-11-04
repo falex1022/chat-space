20b9329b7bd0df581ac4dd47f45a1536ee09fb11
falex1022 <56875497+falex1022@users.noreply.github.com>
2019-11-04 19:34:08 +0900

    chat-space_test

---
+++ spec/factories/messages.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :message do
+    content {Faker::Lorem.sentence}
+    image {File.open("#{Rails.root}/spec/fixtures/test_image.jpg")}
+    user
+    group
+  end
+end
